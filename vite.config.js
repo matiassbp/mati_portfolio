e0466764134c72b3e8ac781280d0e97b0c630f65
Matias Benoni <matiasbenonip@gmail.com>
2025-02-18 00:50:09 -0300

    fix: modificacion en el comando del deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mati_portfolio/',
   plugins: [
     tailwindcss(),
     react(),
